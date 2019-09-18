dffd45532ffe733b6bff3de61cfe738c7637dcfa
Alex Farmer <farmale10@gmail.com>
2019-09-18 13:23:04 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,44 +1,21 @@
 
 
-def Book
-   
-   def initialize(title)
-     @title = title
-   end
-
-  def title 
-    @title 
-  end 
-
-  def author=(author)
-    @author = author
-  end
-  
-  def author 
-    @author
-  end 
-  
-  def page_count=(num)
-    @page_count = num
+class Book
+  attr_accessor :author, :page_count, :genre
+ 
+  def initialize(title)
+    @title = title
   end
  
-  def page_count
-    @page_count
-  end
-  
-  def genre=(genre)
-    @genre = genre
+  def title
+    @title
   end
  
-  def genre
-    @genre
-  end
-  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-
-end 
+ 
+end
 
 
 
