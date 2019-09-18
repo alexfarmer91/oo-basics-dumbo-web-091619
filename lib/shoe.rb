3b81319de2f24f32df5e93ea397cf21e20683345
Alex Farmer <farmale10@gmail.com>
2019-09-18 13:24:52 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
 # Make your shoe class here!
+
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+ 
+  def initialize(brand)
+    @brand = brand
+  end
+ 
+  def brand
+    @brand
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
